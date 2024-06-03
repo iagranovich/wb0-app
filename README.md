51a07e5de4205f5e47a1381f507b0bbf831f2414
Ian Agranovich <agranovich.ian@gmail.com>
2024-06-03 03:40:01 +0300

    migrations

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# Docker
+# Help
+
+## Docker
 
 ***Run docker compose***
 ```shell
 docker compose -f deploy/docker-compose.yaml --project-directory ./ up -d
+```
+
+## Migrations
+
+***Make migration files***
+```shell
+migrate create -ext sql -dir migrations <migration_mame>
+```
+***Migration up***
+```shell
+migrate -path migrations -database postgres://<user:password>@localhost:5432/<db name>?sslmode=disable up
 ```
---
+++ migrations/20240602002529_initial_schema.up.sql
@@ -0,0 +1,53 @@
+CREATE TABLE orders (
+    order_uid VARCHAR(255) PRIMARY KEY,
+    track_number VARCHAR(255) UNIQUE,
+    entry VARCHAR(255),
+    locale VARCHAR(255),
+    internal_signature VARCHAR(255),
+    customer_id VARCHAR(255),
+    delivery_service VARCHAR(255),
+    shardkey INT,
+    sm_id INT,
+    date_created TIMESTAMP,
+    oof_shard INT
+);
+
+CREATE TABLE deliveries (
+    order_uid VARCHAR(255) REFERENCES orders(order_uid),
+    name VARCHAR(255),
+    phone VARCHAR(255),
+    zip VARCHAR(255),
+    city VARCHAR(255),
+    address VARCHAR(255),
+    region VARCHAR(255),
+    email VARCHAR(255),
+    PRIMARY KEY (order_uid)
+);
+
+CREATE TABLE payments (
+    order_uid VARCHAR(255) REFERENCES orders(order_uid),
+    transaction VARCHAR(255),
+    currency VARCHAR(255),
+    provider VARCHAR(255),
+    amount INT,
+    payment_dt INT,
+    bank VARCHAR(255),
+    delivery_cost INT,
+    goods_total INT,
+    custom_fee INT,
+    PRIMARY KEY (order_uid)
+);
+
+CREATE TABLE items (
+    chrt_id INT PRIMARY KEY,
+    track_number VARCHAR(255) REFERENCES orders(track_number),
+    price INT,
+    rid VARCHAR(255),
+    name VARCHAR(255),
+    sale INT,
+    size VARCHAR(255),
+    total_price INT,
+    nm_id INT,
+    brand VARCHAR(255),
+    status INT
+);
