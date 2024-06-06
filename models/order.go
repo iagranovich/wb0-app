d647af1a8058473224fabb0451bd587628c16b0b
Ian Agranovich <agranovich.ian@gmail.com>
2024-06-06 10:21:33 +0300

    fix order in models.Order

--- models/order.go
+++ models/order.go
@@ -4,6 +4,9 @@ type Order struct {
 	OrderUid          string   `db:"order_uid" json:"order_uid"`
 	TrackNumber       string   `db:"track_number" json:"track_number"`
 	Entry             string   `db:"entry" json:"entry"`
+	Delivery          Delivery `json:"delivery"`
+	Payment           Payment  `json:"payment"`
+	Items             []Item   `json:"items"`
 	Locale            string   `db:"locale" json:"locale"`
 	InternalSignature string   `db:"internal_signature" json:"internal_signature"`
 	CustomerId        string   `db:"customer_id" json:"customer_id"`
@@ -12,7 +15,4 @@ type Order struct {
 	SmId              int      `db:"sm_id" json:"sm_id"`
 	DateCreated       string   `db:"date_created" json:"date_created"`
 	OofShard          string   `db:"oof_shard" json:"oof_shard"`
-	Payment           Payment  `json:"payment"`
-	Items             []Item   `json:"items"`
-	Delivery          Delivery `json:"delivery"`
 }
