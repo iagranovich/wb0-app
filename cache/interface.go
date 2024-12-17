48335397b161f7123490e754d003fb1448c4f12c
yan <yan@111.ru>
2024-12-17 13:28:54 +0000

    fix cache interface

--- cache/interface.go
+++ cache/interface.go
@@ -3,7 +3,7 @@ package cache
 import "wb0-app/models"
 
 type Cache interface {
-	Save(order models.Order)
-	FindByUid(uid string) (models.Order, error)
-	Restore(ps persistantStorage)
+	Save(models.Order)
+	FindByUid(string) (models.Order, error)
+	Restore(persistantStorage)
 }
